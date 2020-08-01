151876148497704c5c6079b0594d5a369235f90d
Eliezer Charles <elicharles@gmail.com>
2020-08-01 09:09:55 -0400

    Initial Creation for Angular app with authorization

---
+++ ChatRoomWithBot/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ChatRoomWithBot.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoomWithBot.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
