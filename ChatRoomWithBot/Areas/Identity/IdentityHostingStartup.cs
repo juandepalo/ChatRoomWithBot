f8160008a688416d2019c4da9ed815a8310ce977
Eliezer Charles <elicharles@gmail.com>
2020-08-02 06:09:35 -0400

    Customize Microsoft Identity and Automapper

---
+++ ChatRoomWithBot/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ChatRoom.Domain;
+using ChatRoom.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ChatRoomWithBot.Areas.Identity.IdentityHostingStartup))]
+namespace ChatRoomWithBot.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Core/ChatRoom.Application/ViewModels/ChatMessageViewModel.cs
+++ Core/ChatRoom.Application/ViewModels/ChatMessageViewModel.cs
@@ -1,13 +1,24 @@
-﻿using System;
+﻿using AutoMapper;
+using ChatRoom.Application.Mappings;
+using ChatRoom.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ChatRoom.Application.ViewModels
 {
-    public class ChatMessageViewModel
+    public class ChatMessageViewModel : IMapFrom<ChatMessage>
     {
         public string NickName { get; set; }
         public string Message { get; set; }
         public DateTime CreationDate { get; set; }
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<ChatMessage, ChatMessageViewModel>()
+                .ForMember(dest => dest.NickName, map => map.MapFrom(s => s.ApplicationUser.NickName ?? s.ApplicationUser.UserName))
+                .ForMember(dest => dest.Message, map => map.MapFrom(s => s.Message))
+                .ForMember(dest => dest.CreationDate, map => map.MapFrom(s => s.CreationDate));
+
+        }
     }
 }
